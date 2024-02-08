03fb71e2fc65dd3bf67a5cfd1ad9de00bcfd196e
codedspirit <viradvt@gmail.com>
2024-02-07 19:00:14 -0600

    Commit del dia de hoy

--- Manejo de Presupuestos/Models/TipoCuenta.cs
+++ Manejo de Presupuestos/Models/TipoCuenta.cs
@@ -5,11 +5,33 @@ namespace Manejo_de_Presupuestos.Models
     public class TipoCuenta
     {
         public int Id { get; set; }
-        [Required(ErrorMessage ="El campo \"{0}\" es requerido")]
-        [StringLength(maximumLength: 30, MinimumLength =3, ErrorMessage = "La longitud del campo \"{0}\" debe de estar entre {2} caracteres y {1} caracteres")]
-        [Display(Name ="Nombre del tipo de la cuenta a crear")]
-        public string Nombre { get; set;}
+
+        [Required( ErrorMessage = "Por favor, introduce el nombre del tipo de cuenta." )]
+        [StringLength( maximumLength: 30, MinimumLength = 3, ErrorMessage = "El nombre del tipo de cuenta debe tener entre 3 y 30 caracteres." )]
+        [Display( Name = "Nombre del tipo de la cuenta a crear" )]
+        public string Nombre { get; set; }
+
+        [Required( ErrorMessage = "El ID del usuario es obligatorio." )]
         public int UsuarioId { get; set; }
+
+        [Required( ErrorMessage = "Por favor, introduce un número de orden." )]
+        [Range( 1, int.MaxValue, ErrorMessage = "El número de orden debe ser un número positivo." )]
         public int Orden { get; set; }
+
+        [Required( ErrorMessage = "Por favor, introduce un correo electrónico." )]
+        [EmailAddress( ErrorMessage = "Por favor, introduce un correo electrónico válido." )]
+        public string Email { get; set; }
+
+        [Required( ErrorMessage = "Por favor, introduce tu edad." )]
+        [Range( minimum: 18, maximum: 120, ErrorMessage = "La edad debe estar entre 18 y 120 años." )]
+        public int Edad { get; set; }
+
+        [Required( ErrorMessage = "Por favor, introduce una URL." )]
+        [Url( ErrorMessage = "Por favor, introduce una URL válida." )]
+        public string URL { get; set; }
+
+        [CreditCard( ErrorMessage = "Por favor, introduce un número de tarjeta de crédito válido." )]
+        [Display( Name = "Tarjeta de crédito" )]
+        public string TarjetaDeCredito { get; set; }
     }
 }
